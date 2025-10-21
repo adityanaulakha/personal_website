9ab7960f9c694931419b028989ed0ce9d9036c73
adityanaulakha <naulakhaaditya4@gmail.com>
2025-10-22 02:12:19 +0530

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Skills from './components/Skills';
 import Experience from './components/Experience';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
-import Loader from './components/Loader.jsx';
+import Loader from './components/Loader';
 
 
 function App() {
